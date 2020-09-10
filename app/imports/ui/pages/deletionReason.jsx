063fc398117fec87db135706d5e17bfa95fe8f53
James Bennett <46543890+Bennettjames649@users.noreply.github.com>
2020-09-09 16:40:21 -1000

    pathing + form
    deletion form still doesnt send data anywhere

--- app/imports/ui/pages/deletionReason.jsx
+++ app/imports/ui/pages/deletionReason.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import Form, {  Grid } from 'semantic-ui-react';
+import { Form, Grid, Button, Dropdown } from 'semantic-ui-react';
+import { NavLink } from 'react-router-dom';
 
 class DeletionReason extends React.Component {
   render() {
     return (
-        <div style={{ backgroundColor: '#C4C4C4' }}>
+        <div style={ {backgroundColor: '#C4C4C4' } }>
           <Grid container centered>
+            <Grid.Column>
             <Form>
-              <Form.Field>
-                <label>Tell us why you're leaving!</label>
-                <input />
-              </Form.Field>
+              <Form.TextArea label="Tell us why you're leaving!"/>
+              <Button className="negative ui button" as={NavLink} exact to="/signout">Confirm Deletion</Button>
             </Form>
+              </Grid.Column>
           </Grid>
         </div>
     );
